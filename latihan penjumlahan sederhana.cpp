47c27480557011ee3d4dd89249d3ef7ecfc97f50
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-10 12:59:13 +0630

    Add files via upload

---
+++ latihan penjumlahan sederhana.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a;
+    cout << "masukan a bilangan = ";
+    cin >> a;
+
+    int b;
+    cout << "masukan b bilangan = ";
+    cin >> b;
+
+    cout << "=>" << a << "+" << b << endl;
+    cout << "Hasil penjumlahan = " << a+b << endl;
+
+    return 0;
+}
